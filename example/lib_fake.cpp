4b5c94993c4a062c62169b6bdc5149e064439775
muraak <muraak@dummy.com>
2022-03-26 10:35:12 +0900

    add overload fake example

--- example/lib_fake.cpp
+++ example/lib_fake.cpp
@@ -25,3 +25,25 @@ DEFINE_FAKE_VALUE_FUNC_WITH_EXCEPTION_SPEC(int, /*例外指定子なし*/, Ex2_v
 // void型例外指定子付き関数のFake作成例
 // void型関数はFAKE_VOID_MEMBER_FUNCマクロを使用します。
 DEFINE_FAKE_VOID_FUNC_WITH_EXCEPTION_SPEC(noexcept/*例外指定子*/, Ex2_voidFuncA/*関数名*/, int/*引数型（1つ目）*/);
+
+// オーバーロード関数のFake作成例
+// 2つ目以降のオーバーロード関数のFakeを作成するときは、define/undefをつかって
+// 一部のリソースのシンボル名を置換する必要があります。少々手間でごめんなさい。
+// 1つ目のオーバーロード関数のFake定義
+DEFINE_FAKE_VOID_FUNC_WITH_EXCEPTION_SPEC(noexcept, Ex3_valueFuncA, int);
+// ２つ目のオーバーロード関数のFake定義
+#define Ex3_valueFuncA_Fake             Ex3_valueFuncA_ovl1_Fake
+#define Ex3_valueFuncA_fake             Ex3_valueFuncA_ovl1_fake
+#define Ex3_valueFuncA_reset            Ex3_valueFuncA_ovl1_reset
+DEFINE_FAKE_VOID_FUNC_WITH_EXCEPTION_SPEC(noexcept, Ex3_valueFuncA, float);
+#undef Ex3_valueFuncA_Fake 
+#undef Ex3_valueFuncA_fake 
+#undef Ex3_valueFuncA_reset
+// ３つ目のオーバーロード関数のFake定義
+#define Ex3_valueFuncA_Fake             Ex3_valueFuncA_ovl2_Fake
+#define Ex3_valueFuncA_fake             Ex3_valueFuncA_ovl2_fake
+#define Ex3_valueFuncA_reset            Ex3_valueFuncA_ovl2_reset
+DEFINE_FAKE_VOID_FUNC_WITH_EXCEPTION_SPEC(noexcept, Ex3_valueFuncA, int, float);
+#undef Ex3_valueFuncA_Fake 
+#undef Ex3_valueFuncA_fake 
+#undef Ex3_valueFuncA_reset
--- example/lib_fake.h
+++ example/lib_fake.h
@@ -30,4 +30,27 @@ DECLARE_FAKE_VALUE_FUNC_WITH_EXCEPTION_SPEC(int, /*例外指定子なし*/, Ex2_
 // void型関数はFAKE_VOID_MEMBER_FUNCマクロを使用します。
 DECLARE_FAKE_VOID_FUNC_WITH_EXCEPTION_SPEC(noexcept/*例外指定子*/, Ex2_voidFuncA/*関数名*/, int/*引数型（1つ目）*/);
 
+// オーバーロード関数のFake作成例
+// 2つ目以降のオーバーロード関数のFakeを作成するときは、define/undefをつかって
+// 一部のリソースのシンボル名を置換する必要があります。少々手間でごめんなさい。
+// 推奨置換名規則：<元の関数名> + '_' + <置換用の識別名(e.g. ovl1)> + '_' + <Fake|fake|reset>
+// 1つ目のオーバーロード関数のFake宣言
+DECLARE_FAKE_VOID_FUNC_WITH_EXCEPTION_SPEC(noexcept, Ex3_valueFuncA, int);
+// ２つ目のオーバーロード関数のFake宣言
+#define Ex3_valueFuncA_Fake             Ex3_valueFuncA_ovl1_Fake
+#define Ex3_valueFuncA_fake             Ex3_valueFuncA_ovl1_fake
+#define Ex3_valueFuncA_reset            Ex3_valueFuncA_ovl1_reset
+DECLARE_FAKE_VOID_FUNC_WITH_EXCEPTION_SPEC(noexcept, Ex3_valueFuncA, float);
+#undef Ex3_valueFuncA_Fake 
+#undef Ex3_valueFuncA_fake 
+#undef Ex3_valueFuncA_reset
+// ３つ目のオーバーロード関数のFake宣言
+#define Ex3_valueFuncA_Fake             Ex3_valueFuncA_ovl2_Fake
+#define Ex3_valueFuncA_fake             Ex3_valueFuncA_ovl2_fake
+#define Ex3_valueFuncA_reset            Ex3_valueFuncA_ovl2_reset
+DECLARE_FAKE_VOID_FUNC_WITH_EXCEPTION_SPEC(noexcept, Ex3_valueFuncA, int, float);
+#undef Ex3_valueFuncA_Fake 
+#undef Ex3_valueFuncA_fake 
+#undef Ex3_valueFuncA_reset
+
 #endif // LIB_FAKE_H
